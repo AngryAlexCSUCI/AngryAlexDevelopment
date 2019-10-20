448097899d579806b0291af6cb0483bf1443fed0
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2019-10-20 10:42:29 -0700

    Deleting buggy from scene so it can be spawned when the user joins the match, enabling camera to follow spawned cars

--- AngryAlexReborn/Assets/Scripts/CameraController.cs
+++ AngryAlexReborn/Assets/Scripts/CameraController.cs
@@ -7,59 +7,59 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public float zValue = -10f;
 
-//    public bool isLocalPlayer = false;
+    public bool isLocalPlayer = false;
 
     void Awake()
     {
-//        if (!isLocalPlayer)
-//        {
-//            return;
-//        }
-//
-//        if (!target)
-//        {
-//            target = GameObject.FindGameObjectWithTag("Player").transform;
-//        }
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (!target)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-//        if (!isLocalPlayer)
-//        {
-//            return;
-//        }
+        if (!isLocalPlayer)
+        {
+            return;
+        }
 
-//        if (!target)
-//        {
-//            target = GameObject.FindGameObjectWithTag("Player").transform;
-//        }
+        if (!target)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-//        if (!isLocalPlayer)
-//        {
-//            return;
-//        }
-//
-//        if (!target)
-//        {
-//            target = GameObject.FindGameObjectWithTag("Player").transform;
-//        }
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (!target)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
 
         transform.position = new Vector3(target.position.x, target.position.y, zValue);
     }
 
-//    public void setTarget(Transform _target)
-//    {
-//        target = _target;
-//        transform.position = new Vector3(target.position.x, target.position.y, zValue);
-//    }
-//
-//    public void isLocal(bool value)
-//    {
-//        isLocalPlayer = value; 
-//    }
+    public void setTarget(Transform _target)
+    {
+        target = _target;
+        transform.position = new Vector3(target.position.x, target.position.y, zValue);
+    }
+
+    public void isLocal(bool value)
+    {
+        isLocalPlayer = value;
+    }
 }
