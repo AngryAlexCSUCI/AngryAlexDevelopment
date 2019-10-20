0729a86326aeb1611185e9757571a11ef967acf5
lampson.nguyen <lampson.nguyen410@myci.csuci.edu>
2019-10-20 14:19:33 -0700

    Move the HealthBar to top leff corner

--- AngryAlexReborn/Assets/Scripts/SlowWhenContact.cs
+++ AngryAlexReborn/Assets/Scripts/SlowWhenContact.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SlowWhenContact : MonoBehaviour
 {
-     
+    CarController gameObj;
     private void OnTriggerEnter2D(Collider2D collision)
-
     {
-        Debug.Log("Touch the Mud");
-
-        var gameObj = collision.gameObject.GetComponent<CarController>() as CarController;
-
         Debug.Log("Slow the car");
+        gameObj = collision.gameObject.GetComponent<CarController>() as CarController;
+ 
+        gameObj.velocity = 10;
+        Debug.Log("Slow the car by 20");
+         
+        Invoke("ChangeBackVelocity", 3);
 
-        gameObj.velocity -= 20;
-       
     }
 
+    private void ChangeBackVelocity()
+    {
+
+        gameObj.velocity += 70;
+        Debug.Log("Faster the car by 20");
+
+        gameObj = null;
+    }
 }
