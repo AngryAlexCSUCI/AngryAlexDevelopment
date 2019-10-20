311be7d60949992358d704b77e7140d57c87b198
Gabriel Orellana <gabriel.orellana782@myci.csuci.edu>
2019-10-20 10:16:07 -0700

    Initial commit of vehicle selection scene

---
+++ AngryAlexReborn/Assets/Scripts/SelectorManager.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorManager : MonoBehaviour
+{
+    public GameObject Car;
+    public GameObject Truck;
+    public GameObject Motorcycle;
+    public GameObject Cannon;
+    public GameObject MachineGun;
+    public GameObject Lazer;
+    public GameObject MotorcycleCannon;
+    public GameObject MotorcycleMachineGun;
+    public GameObject MotorcycleLazer;
+    public GameObject CurrentWeapon;
+
+    private Vector2 VehiclePosition;
+    private Vector2 CarWeaponPosition;
+    private Vector2 TruckWeaponPosition;
+    private Vector2 MotorcycleWeaponPosition;
+    private Vector2 Offscreen;
+
+    private int _vehicleNumber = 1, _weaponNumber = 1;
+
+    private SpriteRenderer _carRender, _truckRender, _motorcycleRender, _lazerRender, _machineGunRender, _cannonRender, 
+        _motorcycleCannonRender, _motorcycleMachinegunRender, _motorcycleLazerRender, _currentWeaponRender;
+
+    private Dictionary<int, Vector2> WeaponPositions = new Dictionary<int, Vector2>();
+    private Dictionary<int, Dictionary<int, GameObject>> Weapons = new Dictionary<int, Dictionary<int, GameObject>>();
+    private Dictionary<int, GameObject> MotorcycleWeapons = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> VehicleWeapons = new Dictionary<int, GameObject>();
+    private Dictionary<int, Dictionary<int, SpriteRenderer>> WeaponRenderers = new Dictionary<int, Dictionary<int, SpriteRenderer>>();
+
+    private Dictionary<int, SpriteRenderer> motorcycleWeaponRenderers = new Dictionary<int, SpriteRenderer>();
+    private Dictionary<int, SpriteRenderer> weaponRenderers = new Dictionary<int, SpriteRenderer>();
+
+    private void Awake()
+    {
+        VehiclePosition = Car.transform.position;
+        CarWeaponPosition = Cannon.transform.position;
+        TruckWeaponPosition = Cannon.transform.position;
+        TruckWeaponPosition.y = TruckWeaponPosition.y - .6f;
+        MotorcycleWeaponPosition = Cannon.transform.position;
+        MotorcycleWeaponPosition.y = TruckWeaponPosition.y + 1f;
+        MotorcycleWeaponPosition.x = TruckWeaponPosition.x - .04f;
+        Offscreen = Truck.transform.position;
+        _carRender = Car.GetComponent<SpriteRenderer>();
+        _truckRender = Truck.GetComponent<SpriteRenderer>();
+        _motorcycleRender = Motorcycle.GetComponent<SpriteRenderer>();
+        _cannonRender = Cannon.GetComponent<SpriteRenderer>();
+        _machineGunRender = MachineGun.GetComponent<SpriteRenderer>();
+        _lazerRender = Lazer.GetComponent<SpriteRenderer>();
+        _motorcycleCannonRender = MotorcycleCannon.GetComponent<SpriteRenderer>();
+        _motorcycleMachinegunRender = MotorcycleMachineGun.GetComponent<SpriteRenderer>();
+        _motorcycleLazerRender = MotorcycleLazer.GetComponent<SpriteRenderer>();
+
+        WeaponPositions.Add(1, CarWeaponPosition);
+        WeaponPositions.Add(2, TruckWeaponPosition);
+        WeaponPositions.Add(3, MotorcycleWeaponPosition);
+
+        VehicleWeapons.Add(1, Cannon);
+        VehicleWeapons.Add(2, MachineGun);
+        VehicleWeapons.Add(3, Lazer);
+
+        MotorcycleWeapons.Add(1, MotorcycleCannon);
+        MotorcycleWeapons.Add(2, MotorcycleMachineGun);
+        MotorcycleWeapons.Add(3, MotorcycleLazer);
+
+        Weapons.Add(1, VehicleWeapons);
+        Weapons.Add(2, VehicleWeapons);
+        Weapons.Add(3, MotorcycleWeapons);
+        
+        weaponRenderers.Add(1, _cannonRender);
+        weaponRenderers.Add(2, _machineGunRender);
+        weaponRenderers.Add(3, _lazerRender);
+
+        motorcycleWeaponRenderers.Add(1, _motorcycleCannonRender);
+        motorcycleWeaponRenderers.Add(2, _motorcycleMachinegunRender);
+        motorcycleWeaponRenderers.Add(3, _motorcycleLazerRender);
+
+        WeaponRenderers.Add(1, weaponRenderers);
+        WeaponRenderers.Add(2, weaponRenderers);
+        WeaponRenderers.Add(3, motorcycleWeaponRenderers);
+
+        CurrentWeapon = Cannon;
+        _currentWeaponRender = _cannonRender;
+
+        _truckRender.enabled = false;
+        _motorcycleRender.enabled = false;
+        _machineGunRender.enabled = false;
+        _lazerRender.enabled = false;
+        _motorcycleCannonRender.enabled = false;
+        _motorcycleMachinegunRender.enabled = false;
+        _motorcycleLazerRender.enabled = false;
+    }
+
+    public void NextVehicle()
+    {
+        switch (_vehicleNumber)
+        {
+            case 1:
+                _carRender.enabled = false;
+                _truckRender.enabled = true;
+                CurrentWeapon.transform.position = TruckWeaponPosition;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                _vehicleNumber++;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                break;
+            case 2:
+                _truckRender.enabled = false;
+                _motorcycleRender.enabled = true;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                _vehicleNumber++;
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = MotorcycleWeaponPosition;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                break;
+            case 3:
+                _carRender.enabled = true;
+                _motorcycleRender.enabled = false;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                _vehicleNumber = 1;
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = CarWeaponPosition;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PreviousVehicle()
+    {
+        switch (_vehicleNumber)
+        {
+            case 1:
+                _carRender.enabled = false;
+                _motorcycleRender.enabled = true;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                CurrentWeapon = Weapons[3][_weaponNumber];
+                CurrentWeapon.transform.position = MotorcycleWeaponPosition;
+                _currentWeaponRender = WeaponRenderers[3][_weaponNumber];
+                WeaponRenderers[_vehicleNumber+2][_weaponNumber].enabled = true;
+                _vehicleNumber = 3;
+                break;
+            case 2:
+                _truckRender.enabled = false;
+                _carRender.enabled = true;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                _vehicleNumber--;
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = CarWeaponPosition;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                break;
+            case 3:
+                _motorcycleRender.enabled = false;
+                _truckRender.enabled = true;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = false;
+                _vehicleNumber--;
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = TruckWeaponPosition;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void NextWeapon()
+    {
+        switch (_weaponNumber)
+        {
+            case 1:
+                _currentWeaponRender.enabled = false;
+                _weaponNumber++;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = WeaponPositions[_vehicleNumber];
+                break;
+            case 2:
+                _currentWeaponRender.enabled = false;
+                _weaponNumber++;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = WeaponPositions[_vehicleNumber];
+                break;
+            case 3:
+                _currentWeaponRender.enabled = false;
+                WeaponRenderers[_vehicleNumber][1].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][1];
+                _weaponNumber = 1;
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                CurrentWeapon.transform.position = WeaponPositions[_vehicleNumber];
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PreviousWeapon()
+    {
+        switch (_weaponNumber)
+        {
+            case 1:
+                _currentWeaponRender.enabled = false;
+                _weaponNumber = 3;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                break;
+            case 2:
+                _currentWeaponRender.enabled = false;
+                _weaponNumber--;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];                
+                break;
+            case 3:
+                _currentWeaponRender.enabled = false;
+                _weaponNumber--;
+                WeaponRenderers[_vehicleNumber][_weaponNumber].enabled = true;
+                _currentWeaponRender = WeaponRenderers[_vehicleNumber][_weaponNumber];
+                CurrentWeapon = Weapons[_vehicleNumber][_weaponNumber];
+                break;
+            default:
+                break;
+        }
+    }
+
+}
