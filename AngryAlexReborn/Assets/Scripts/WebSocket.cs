f0bb7edeeca7071262ab76df939fd71b7be28c21
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2019-10-20 20:04:41 -0700

    adding web socket plug in and stuff

---
+++ AngryAlexReborn/Assets/Scripts/WebSocket.cs
@@ -0,0 +1,112 @@
+//Copyright (c) 2015 Gumpanat Keardkeawfa
+//Licensed under the MIT license
+
+//Websocket C# for UnityWebgl
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEditor;
+
+public class WebSocket : MonoBehaviour
+{
+
+    #region WebSockets Implement
+    Queue<string> recvList = new Queue<string>(); //keep receive messages
+
+    [DllImport("__Internal")]
+    private static extern string Hello(); //test javascript plugin
+
+    [DllImport("__Internal")]
+    private static extern void InitWebSocket(string url); //create websocket connection
+
+    [DllImport("__Internal")]
+    private static extern int State(); //check websocket state
+
+    [DllImport("__Internal")]
+    private static extern void Send(string message); //send message
+
+    [DllImport("__Internal")]
+    private static extern void Close(); //close websocket connection
+
+    //For Receive Message, this function was call by plugin, we need to keep this name.
+    void RecvString(string message)
+    {
+        recvList.Enqueue(message);   //We will put the new message to the recvList queue.
+    }
+
+    //For Receive Message, this function was call by plugin, we need to keep this name.
+    void ErrorString(string message)
+    {
+        //We can do the same as RecvString here.
+    }
+    #endregion
+
+    void Start()
+    {
+        //        var currentPath = Environment.GetEnvironmentVariable("PATH",
+        //            EnvironmentVariableTarget.Process);
+        //        print("Current path: " + currentPath);
+        //
+        //        var dllPath = Application.dataPath
+        //                      + Path.AltDirectorySeparatorChar + "Plugins";
+        //        print("DLL path: " + dllPath);
+        //
+        //        if (currentPath != null && currentPath.Contains(dllPath) == false)
+        //            Environment.SetEnvironmentVariable("PATH", currentPath + Path.PathSeparator
+        //                                                                   + dllPath, EnvironmentVariableTarget.Process);
+        //        print(Environment.GetEnvironmentVariable("PATH"));
+
+        print("Starting web socket..");
+        print(Hello());        //We start with testing plugin
+        
+        StartCoroutine("RecvEvent");    //Then run the receive message loop
+    }
+
+    //Receive message loop function
+    IEnumerator RecvEvent()
+    {
+        print("Starting coroutine.");
+        InitWebSocket("wss://ec2-54-165-152-15.compute-1.amazonaws.com:3000"); //First we create the connection.
+        while (true)
+        {
+            if (recvList.Count > 0)
+            {         //When our message queue has string coming.
+                Dispatch(recvList.Dequeue());    //We will dequeue message and send to Dispatch function.
+            }
+            yield return null;
+        }
+    }
+
+    //You can implement your game method here :)
+    void Dispatch(string msg)
+    {
+        string[] splits = msg.Split(' ');
+        switch (splits[0])
+        {
+            case "turn":
+                //DO SOMETHING
+                Debug.LogWarning("Turn " + splits[1]);
+                break;
+            default:
+                Debug.Log("Dispatch : " + msg);
+                break;
+        }
+    }
+
+    //For UI, we defined it here
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 30), "Hello,World"))
+            Send("Hello there!");
+//
+//        if (GUI.Button(new Rect(10, 60, 150, 30), "Turn Right"))
+//            Send("turn r");
+//
+//        if (GUI.Button(new Rect(10, 110, 150, 30), "Turn Left"))
+//            Send("turn l");
+    }
+}
