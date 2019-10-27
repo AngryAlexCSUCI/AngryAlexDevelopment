6526d692753853539cbe1b5fcd8ac8818cb5446e
Gabriel Orellana <gabriel.orellana782@myci.csuci.edu>
2019-10-27 10:59:30 -0700

    Update to store vehicle selection

--- AngryAlexReborn/Assets/Scripts/UsernameSceneScripts/Player.cs
+++ AngryAlexReborn/Assets/Scripts/UsernameSceneScripts/Player.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class Player
 {
     private static string userName;
-    
+    private static Tuple<int, int> vehicleLoadout;
+
     public static string UserName 
     {
         get 
@@ -17,4 +19,6 @@ public static class Player
             userName = value;
         }
     }
+
+    public static Tuple<int, int> VehicleLoadout { get => vehicleLoadout; set => vehicleLoadout = value; }
 }
