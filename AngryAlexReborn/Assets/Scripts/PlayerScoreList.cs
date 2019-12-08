127a89d30974b5c75033bdc9f1faed34e6598532
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2019-12-08 13:21:30 -0800

    make sure not to display empty or weird names in leaderboard list

--- AngryAlexReborn/Assets/Scripts/PlayerScoreList.cs
+++ AngryAlexReborn/Assets/Scripts/PlayerScoreList.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -45,13 +46,19 @@ public class PlayerScoreList : MonoBehaviour
         string[] names = leaderboardManager.GetPlayerNames("kills");
 
         int place = 1;
-        for (int i = 0; i < 10; ++i)
+        for (int i = 0; i < names.Length && place <= 10; ++i)
         {
+            if (String.IsNullOrEmpty(names[i]))
+            {
+                continue;
+            }
             GameObject go = (GameObject)Instantiate(playerScoreEntryPrefab);
             go.transform.SetParent(this.transform);
+            string score = leaderboardManager.GetScore(names[i], "kills").ToString();
+            Debug.Log("Leaderboard update for: " + names[i] + " to place " + place + " with kills " + score);
             go.transform.Find("Player Name").GetComponent<Text>().text = names[i];
             go.transform.Find("Place").GetComponent<Text>().text = place.ToString();
-            go.transform.Find("Score").GetComponent<Text>().text = leaderboardManager.GetScore(name, "kills").ToString(); ;
+            go.transform.Find("Score").GetComponent<Text>().text = score;
             ++place;
         }
 
